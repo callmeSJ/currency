063c036fd5c5a7132cd606a7fed2b54d2982544d
SJ <shijie7575@foxmail.com>
2016-07-04 09:35:35 +0800

    Initial Commit

---
+++ CurrentExchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrentExchange
+//
+//  Created by SJ on 16/7/4.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrentExchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrentExchange
+//
+//  Created by SJ on 16/7/4.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
